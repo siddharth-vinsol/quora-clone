d9294890c9dca3092c6c24b6e916d5389579292b
siddharth <siddharth.pawar@vinsol.com>
2022-08-31 13:43:46 +0530

    Add seed data for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.delete_all
+User.create(name: 'user1', email: 'user1@user.com', password: 'password', password_confirmation: 'password', verified: SecureRandom.uuid)
+User.create(name: 'user2', email: 'user2@user.com', password: 'password', password_confirmation: 'password', verified: SecureRandom.uuid)
+User.create(name: 'user3', email: 'user3@user.com', password: 'password', password_confirmation: 'password', verified: SecureRandom.uuid)
