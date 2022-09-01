53a9fc3acf8e86dc95c455567aac30444a0fbf48
siddharth <siddharth.pawar@vinsol.com>
2022-09-01 17:03:10 +0530

    Add signup button on login page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
     if @user.save
       redirect_to login_path, notice: 'Thank you for signing up, a verification email has been sent to your account.'
     else
-      render :signup, status: :unprocessable_entity
+      render :show, status: :unprocessable_entity
     end
   end
 
