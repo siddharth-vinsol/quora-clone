fd2f3cd9a160eb720c3019e65eb920e1e289da51
siddharth <siddharth.pawar@vinsol.com>
2022-09-02 12:03:55 +0530

    Update authentication with cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :auto_login
   before_action :authorize
 
   def authorize
-    unless @user = User.find_by(id: session[:user_id])
+    unless set_current_user
       redirect_to login_path, notice: 'Please log in before continuing'
     end
   end
 
-  private def auto_login
-    if !session[:user_id] && cookies.encrypted[:login]
-      session[:user_id] = cookies.encrypted[:login]
-      redirect_to user_path
-    end
+  private def set_current_user
+    @user = User.find_by(id: cookies.encrypted[:user_id])
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,11 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       if @user.verified_at?
-        cookies.encrypted[:login] = { value: @user.id, expires: QuoraClone::Session::COOKIE_EXPIRATION_TIME.from_now } if params[:remember_me] == '1'
-        session[:user_id] = @user.id
+        cookies.encrypted[:user_id] = { 
+          value: @user.id,
+          expires: params[:remember_me] == '1' ? QuoraClone::Session::COOKIE_EXPIRATION_TIME.from_now : nil
+        }
+
         redirect_to user_path
       else
         redirect_to login_url, notice: 'Please verify your account using link sent on your email before moving further'
@@ -20,8 +23,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    reset_session
-    cookies.delete(:login)
+    cookies.delete(:user_id)
     redirect_to login_path, notice: 'Logged out successfully'
   end
 end
