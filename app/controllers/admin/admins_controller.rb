1654c01f3fd14e0328bff4953f0d044edc11a27f
siddharth <siddharth.pawar@vinsol.com>
2022-09-07 21:46:33 +0530

    Add disable status to user

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,4 +1,6 @@
 class Admin::AdminsController < Admin::BaseController
+  before_action :set_user, only: [:disable_user_account]
+
   def show
   end
 
@@ -7,5 +9,22 @@ class Admin::AdminsController < Admin::BaseController
   end
 
   def questions
+  end
+
+  def disable_user_account
+    if params[:should_disable] == '1'
+      status = Time.current
+      message = t('notice.user.admin.user_disable_success')
+    else
+      status = nil
+      message = t('notice.user.admin.user_enable_success')
+    end
+
+    @user.change_disable_status(status)
+    redirect_to users_admin_path, notice: message
+  end
+
+  private def set_user
+    redirect_to users_admin_path, notice: t('notice.user.admin.user_not_found') unless @user = User.find_by(id: params[:user_id])
   end
 end
---
+++ db/migrate/20220907154227_add_disabled_at_to_users.rb
@@ -0,0 +1,5 @@
+class AddDisabledAtToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :disabled_at, :timestamp
+  end
+end
