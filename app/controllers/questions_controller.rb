63433bb688a180db8788610258f634102308c1ec
siddharth <siddharth.pawar@vinsol.com>
2022-09-08 12:39:31 +0530

    Add question page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,14 @@
 class QuestionsController < ApplicationController
-  def new
+  before_action :set_question, only: [:show]
+
+  def index
+    @questions = Question.where.not(published_at: nil).where.not(user_id: current_user.id).order('created_at DESC').includes(:user)
   end
 
   def show
+  end
+
+  private def set_question
+    @question = Question.find(params[:id])
   end
 end
