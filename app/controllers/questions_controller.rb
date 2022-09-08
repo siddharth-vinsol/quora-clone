bfe05f752a6199894e98c941f08178ad4089ba51
siddharth <siddharth.pawar@vinsol.com>
2022-09-08 13:03:28 +0530

    Add search field on Questions route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = Question.where.not(published_at: nil).where.not(user_id: current_user.id).order('created_at DESC').includes(:user)
+    @questions = @questions.where('title LIKE ?', '%' + Question.sanitize_sql_like(params[:search_title]) + '%') unless params[:search_title].empty?
   end
 
   def show
