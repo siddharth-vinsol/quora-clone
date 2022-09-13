5827ec2ee6ae5935f7fa3132341b07b77bebde41
siddharth <siddharth.pawar@vinsol.com>
2022-09-13 17:10:13 +0530

    Add change password page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def password
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to profile_path, notice: t('profile_update_success')
@@ -14,6 +17,6 @@ class UsersController < ApplicationController
   end
 
   private def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :password, :password_confirmation)
   end
 end
