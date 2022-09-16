5072e1d29779bdda0f67c2296cbda31a545dfef0
siddharth <siddharth.pawar@vinsol.com>
2022-09-16 10:50:46 +0530

    Add Abuse report to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
   before_action :load_commentable, only: [:create]
 
   def create
-    @new_comment = @commentable.comments.build(content: comment_params[:content], user_id: current_user.id)
+    @new_comment = @commentable.comments.build(content: comment_params[:content], user_id: current_user.id, published_at: Time.current)
 
-    if @new_comment.save
+    if @new_comment.save!
       redirect_to request.referrer, notice: t('comment_added')
     else
       flash[:notice] = @new_comment.errors.full_messages.first
