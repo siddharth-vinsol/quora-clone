541721c23afb2d6433a50ac2208c260e701f5329
siddharth <siddharth.pawar@vinsol.com>
2022-09-21 00:19:43 +0530

    Update question attachment presence check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def render_filelink_if_exists(file)
-    sanitize "<a href=#{rails_blob_path(file, disposition: 'attachment')} >Attached File</a>" if file.signed_id
+    sanitize "<a href=#{rails_blob_path(file, disposition: 'attachment')} >Attached File</a>" if file.present?
   end
 
   def filename_from_file(file)
