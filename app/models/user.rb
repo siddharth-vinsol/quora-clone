94ee757b783c09d02ec12c6c107ee58ab68ab00d
siddharth <siddharth.pawar@vinsol.com>
2022-09-21 11:40:36 +0530

    Add constants for file extensions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  VALID_IMAGE_MIME_TYPES = 'image/png, image/jpeg'
+  VALID_IMAGE_EXTENSIONS = ['.png', '.jpeg']
+
   before_create :generate_confirmation_token
   after_create_commit :send_verification_mail
 
@@ -14,7 +17,7 @@ class User < ApplicationRecord
   validates :password, :password_confirmation, presence: true, if: :setting_password?
   validates :email, uniqueness: true
   validates :email, format: { with: QuoraClone::RegexConstants::EMAIL_REGEX }, allow_blank: true
-  validates :profile_image, attached_file_type: { types: ['.png', '.jpeg'] }, allow_blank: true
+  validates :profile_image, attached_file_type: { types: VALID_IMAGE_EXTENSIONS }, allow_blank: true
 
   def update_password_reset_token
     if update(password_reset_token: TokenHandler.generate_token, reset_password_sent_at: Time.current)
