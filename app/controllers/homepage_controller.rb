d094da4699453579be51e3c19eeed2e5664f28db
siddharth <siddharth.pawar@vinsol.com>
2022-09-22 12:19:40 +0530

    Add follower questions form on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,7 +4,7 @@ class HomepageController < ApplicationController
   before_action :update_search_params, only: [:index], if: :signed_in?
 
   def index
-    @q = Question.includes(:user, :rich_text_content).published_only.by_recently_created.ransack(params[:q])
+    @q = Question.includes(:user, :rich_text_content).published_only.by_recently_created.ransack(search_params)
     @questions = @q.result
   end
 
@@ -13,6 +13,14 @@ class HomepageController < ApplicationController
       params[:q][:user_id_not_eq] = current_user.id
     else
       params[:q] = { user_id_not_eq: current_user.id }
+    end
+  end
+  
+  private def search_params
+    if params[:q].present?
+      ransack_params = params.require(:q).permit(:title_cont, :user_id_not_eq, :user_id_eq_any, topics_name_eq_any: [])
+      ransack_params[:user_id_eq_any] = ransack_params[:user_id_eq_any].try(:split)
+      ransack_params
     end
   end
 end
