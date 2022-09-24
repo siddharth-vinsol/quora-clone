df498e0cf09f0552127840c73ecb93140922b718
siddharth <siddharth.pawar@vinsol.com>
2022-09-24 12:50:31 +0530

    Add notifications for Answer and Comment

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,12 +4,15 @@ class Answer < ApplicationRecord
   include CommentsHandler
   include AbuseReportsHandler
   include AutoScrollHandler
+  include NotificationsHandler
 
+  after_create :generate_notifications
   after_create_commit :send_answer_posted_mail
 
   belongs_to :user
   belongs_to :question
   has_rich_text :content
+  has_many :notifications, as: :notifiable
   
   validates :content, presence: true
   
@@ -18,6 +21,12 @@ class Answer < ApplicationRecord
   private def send_answer_posted_mail
     if user_id != question.user_id
       UserMailer.answer_posted(id).deliver_later
+    end
+  end
+
+  private def generate_notifications
+    if user_id != question.user_id
+      notifications.create(user: question.user, content: 'Someone posted an answer on your question.', redirect_link: question_path(question.permalink))
     end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,27 @@ class Comment < ApplicationRecord
   include VoteHandler
   include AbuseReportsHandler
   include AutoScrollHandler
+  include NotificationsHandler
 
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+  has_many :notifications, as: :notifiable
   
   validates :content, presence: true
+
+  after_create :generate_notifications
+
+  private def generate_notifications
+    if user_id != commentable.user_id
+      if commentable_type == 'Question'
+        content = 'Someone commented on your question.'
+        redirect_link = question_path(commentable.permalink)
+      else
+        content = 'Someone commented on your answer.'
+        redirect_link = question_path(commentable.question.permalink)
+      end
+
+      notifications.create(user: commentable.user, content: content, redirect_link: redirect_link)
+    end
+  end
 end
