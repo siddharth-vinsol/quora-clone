1891209ec79bb6cc9308157b9a3adcdbdc81cb88
siddharth <siddharth.pawar@vinsol.com>
2022-09-24 10:49:23 +0530

    Add scroll handler for answers

---
+++ app/javascript/scripts/auto_scroll_handler.js
@@ -0,0 +1,21 @@
+class AutoScrollHandler {
+  constructor(scrollToElement) {
+    this.scrollToElement = scrollToElement
+  }
+
+  init() {
+    if (this.scrollToElement) {
+      $('html, body').animate({
+        scrollTop: this.scrollToElement.offset().top
+      }, 1000);
+      this.scrollToElement.addClass('highlight')
+    }
+  }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('scroll_to')) {
+  const elementId = urlParams.get('scroll_to')
+  let autoScrollHandler = new AutoScrollHandler($(`*[data-scroll_id="${elementId}"]`))
+  autoScrollHandler.init();
+}
