86f97257de26830d017aa737adbe421291115e94
siddharth <siddharth.pawar@vinsol.com>
2022-09-25 15:02:00 +0530

    Add homepage notification pop for new question

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.1.min.js", preload: true
 pin 'select2', to: 'select2.js'
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
