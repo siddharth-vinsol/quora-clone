5c1cfd23e252bfe3c44271e17270e3b16905fb08
siddharth <siddharth.pawar@vinsol.com>
2022-09-26 16:57:20 +0530

    Add feed api endpoint

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :total_upvotes, :total_downvotes, :permalink
+  attributes :id, :title, :total_upvotes, :total_downvotes, :permalink, :topic_list
   belongs_to :user
   has_many :answers
   has_many :comments
--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'feed', to: 'users#feed', format: true, constraints: { format: :json }
       resources :topics, only: [:index, :show], param: :topic
     end
   end
