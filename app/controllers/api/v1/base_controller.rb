45c2d3b4a3bf6c101aaad9ed0f2e6805e9d8b29d
siddharth <siddharth.pawar@vinsol.com>
2022-09-26 16:57:20 +0530

    Add feed api endpoint

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::BaseController < ApplicationController
+  before_action :authorize
+
+  def authorize
+    unless current_user
+      render json: { error: t('invalid_auth_token') }, status: :unprocessable_entity
+    end
+  end
+
+  def current_user
+    @logged_in_user ||= User.find_by(auth_token: params[:token])
+  end
 end
