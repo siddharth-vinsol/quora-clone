4f71a6c0e146f0831f94ed3775681a1c5d734f55
siddharth <siddharth.pawar@vinsol.com>
2022-09-28 16:44:18 +0530

    Add comment delete button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :load_commentable, only: [:create]
+  before_action :set_comment, only: [:destroy]
+  before_action :validate_current_user_resource, only: [:destroy] 
 
   def create
     @new_comment = @commentable.comments.build(content: comment_params[:content], user_id: current_user.id, published_at: Time.current)
@@ -12,11 +14,27 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    if @comment.destroy
+      redirect_to request.referrer, notice: t('comment_deleted')
+    else
+      render 'questions/show', status: :unprocessable_entity
+    end
+  end
+
   private def load_commentable
     @commentable = comment_params[:commentable_type].constantize.find(comment_params[:commentable_id])
   end
 
   private def comment_params
     params.require(:comment).permit(:commentable_id, :commentable_type, :content)
+  end
+
+  private def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  private def resource
+    @comment
   end
 end
